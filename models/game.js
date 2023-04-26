8b631e846a183062dddd590a76ee739f34a3218e
Supreeth <psaisupreeth10111997@gmail.com>
2023-04-26 15:23:38 -0500

    Done

--- models/game.js
+++ models/game.js
@@ -1,7 +1,14 @@
 const mongoose = require("mongoose")
 const gameSchema = mongoose.Schema({
-game_name: String,
-version: Number,
+game_name: {
+    type: String,
+    required: true,
+    match: /^[a-zA-Z]+$/
+  },
+version: {
+    type:Number,
+    min: 1,
+    max: 10000 } ,
 price: String
 })
 module.exports = mongoose.model("game",
--- routes/game.js
+++ routes/game.js
@@ -2,6 +2,18 @@ var express = require('express');
 const game_controlers= require('../controllers/game');
 var router = express.Router();
 
+// A little function to check if we have an authorized user and continue on 
+// or 
+// redirect to login. 
+const secured = (req, res, next) => { 
+    if (req.user){ 
+      return next(); 
+    } 
+    req.session.returnTo = req.originalUrl; 
+    res.redirect("/login"); 
+  } 
+
+
 /* GET games */
 router.get('/', game_controlers.game_view_all_Page );
 
@@ -9,12 +21,12 @@ router.get('/', game_controlers.game_view_all_Page );
 router.get('/detail', game_controlers.game_view_one_Page);
 
 /* GET create game page */
-router.get('/create', game_controlers.game_create_Page);
+router.get('/create',secured, game_controlers.game_create_Page);
 
 /* GET create update page */
-router.get('/update', game_controlers.game_update_Page);
+router.get('/update',secured, game_controlers.game_update_Page);
 
 /* GET delete game page */
-router.get('/delete', game_controlers.game_delete_Page);
+router.get('/delete',secured, game_controlers.game_delete_Page);
 
 module.exports = router;
